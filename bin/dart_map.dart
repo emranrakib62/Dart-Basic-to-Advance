087fc7b3587d46eb9473892fa9b311c6bf5457ea
Emranul Hoque <emranrakib01862@gmail.com>
2025-10-08 00:37:00 +0600

    updated

--- bin/dart_map.dart
+++ bin/dart_map.dart
@@ -2,9 +2,16 @@ void main(){
 
   Map<String,dynamic> student={
 
-
+"name": "Robin",
+    "age":"10",
+    "grade":"1",
+    "ispassed":true
 
 
 
   };
+  print(student);
+  //accessing Values
+  print("${student["name"]}");
+  
 }
