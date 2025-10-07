91541a789a9aa5176eaca41ab1fff151682ebcd1
Emranul Hoque <emranrakib01862@gmail.com>
2025-10-07 22:06:53 +0600

    first commit

---
+++ bin/syntex.dart
@@ -0,0 +1,8 @@
+void main(){
+  var name="dart";
+      print("hello $name");
+
+      //final and const
+  final String name_="dart";//initialized run time
+  const pi=3.1416;//must be initialize at compile time
+}
---
+++ bin/variable_dart.dart
@@ -0,0 +1,16 @@
+void main(){
+  /*var dynamicVar=42;
+  int fixedvar=100;
+  double pi=3.14;
+  bool isfun=true;
+
+  print(dynamicVar);
+  print(fixedvar);
+  print(pi);
+  print(isfun);
+*/
+  String number="42";
+  int parsenumber=int.parse(number);
+  print(parsenumber);
+
+}
