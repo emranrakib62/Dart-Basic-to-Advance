6c51ed184f68054fa6209ae83fc480609a208516
Emranul Hoque <emranrakib01862@gmail.com>
2025-10-08 02:02:04 +0600

    updated

--- bin/dart_map.dart
+++ bin/dart_map.dart
@@ -13,5 +13,15 @@ void main(){
   print(student);
   //accessing Values
   print("${student["name"]}");
-  
+
+  student.remove("age");
+  print(student);
+
+  if (student.containsKey("grade")){
+    print("yes");
+  }else{
+    print("no");
+  }
+  print("key ${student.keys}");
+  print("values ${student.values}");
 }
