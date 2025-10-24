361e91d1c95a521d8a421cb20cddc5b5d4532a19
Emranul Hoque <emranrakib01862@gmail.com>
2025-10-24 22:42:56 +0600

    updated

--- bin/string.dart
+++ bin/string.dart
@@ -7,6 +7,17 @@ void main(){
   String message='$greeting,$name !Welcome to dart programming';
   print(message);
 
+print('\nMulti line string example:');
+String multiline='''This is multi line string in dart
+you can write text across multiple lines.''';
+print('$multiline');
+
+
+print('\nRaw string example:');
+String rawstring=r'this is a raw string.\nNew lines and\t tabs are not processed';
+
+print(rawstring);
+
 
 
 }
