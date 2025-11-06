e07d08263fb983f41377a455d1d9bb6b0a044f8f
Emranul Hoque <emranrakib01862@gmail.com>
2025-11-06 18:20:31 +0600

    updated

--- bin/control_flow.dart
+++ bin/control_flow.dart
@@ -1,16 +1,13 @@
-import "dart:io";
+import 'dart:io';
 
 void main() {
-
   for (int i = 1; i <= 10; i++) {
-    print('number:$i');
+    print('number: $i');
   }
-
 
   for (int i = 2; i < 100; i = i + 2) {
-    print('numbner:$i');
+    print('number: $i');
   }
-
 
   List<String> names = ['hasan', 'bob', 'charlie', 'diane'];
 
@@ -18,42 +15,40 @@ void main() {
     print('name at the index $i: ${names[i]}');
   }
 
-
-  int countdown=5;
-
-  while(countdown<0){
+  int countdown = 5;
+  while (countdown >= 0) {   // fixed condition
     print('countdown: $countdown');
-    countdown++;
+    countdown--;
   }
+  print('Launch!!');
 
-  print('Launch??');
-
-
-
-
-
-  int number=6;
-
-  do{
-    print('this will print even though number>5');
+  int number = 6;
+  do {
+    print('this will print even though number > 5');
     number++;
-  }while(number <=5);
+  } while (number <= 5);
 
-do{
+  // ----------- Fixed Menu Section -----------
+  String? choice;
 
-  print('....simple Menu....');
-  print('1. say hello');
-  print('2. say goodbye');
-  print('3.Exit');
+  do {
+    print('\n....Simple Menu....');
+    print('1. Say Hello');
+    print('2. Say Goodbye');
+    print('3. Exit');
 
-  print('Enter your Choice');
-  choice = stdin.readLineSync();
+    stdout.write('Enter your choice: ');
+    choice = stdin.readLineSync();
+
+    if (choice == '1') {
+      print('Hello!');
+    } else if (choice == '2') {
+      print('Goodbye!');
+    } else if (choice == '3') {
+      print('Exiting...');
+    } else {
+      print('Invalid Choice! Try again.');
+    }
+
+  } while (choice != '3');
 }
-
-
-
-}
-
-
-
-
