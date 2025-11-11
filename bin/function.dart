b9771b150209625ab38ef0ba866143693d40e44c
Emranul Hoque <emranrakib01862@gmail.com>
2025-11-11 20:31:55 +0600

    updated

--- bin/function.dart
+++ bin/function.dart
@@ -1,8 +1,13 @@
 void main(){
-  greet("Mitu",18);
 
+positionalparameter();
 }
 void greet(String name,int Age){
   print("Hello $name how are you,you are $Age years old");
+}
+void positionalparameter(){
+  greet("Mitu",18);
+  greet("Hasan",15);
+  greet("Aliya",20);
 }
 
