1c4ca3d96f868949289a5e4229eb2efda713c936
Emranul Hoque <emranrakib01862@gmail.com>
2025-11-11 20:44:47 +0600

    updated

--- bin/function.dart
+++ bin/function.dart
@@ -1,6 +1,7 @@
 void main(){
 
 positionalparameter();
+nameparameterExample();
 }
 void greet(String name,int Age){
   print("Hello $name how are you,you are $Age years old");
@@ -10,4 +11,22 @@ void positionalparameter(){
   greet("Hasan",15);
   greet("Aliya",20);
 }
+
+void createuser(String name,int Age, String Email){
+  print('Printed user');
+  print('Name:${name ?? 'Not provided'}');
+  print('Age:${Age ?? 'Not provided'}');
+  print('Email:${Email ?? 'Not provided'}');
+
+
+}
+
+void nameparameterExample(){
+  createuser('Emran', 18, 'emran@gmail.com');
+  createuser('Amit', 25, 'amit@gmail.com');
+  createuser('korim', 30, 'korim@gmail.com');
+
+}
+
+
 
