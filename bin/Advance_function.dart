e78613631269ec5076f6aeec7f54fe66fedf3989
Emranul Hoque <emranrakib01862@gmail.com>
2025-11-19 10:35:25 +0600

    updated

---
+++ bin/Advance_function.dart
@@ -0,0 +1,17 @@
+String globalVar = 'I am a Global Variable';
+String getGlobalVar() => globalVar;
+
+void main(){
+
+
+
+}
+
+void anonymousFunctionExample(){
+  print('Anonymous function Example');
+  var greet =(String name){
+    print('hello $name!');
+
+  };
+
+}
