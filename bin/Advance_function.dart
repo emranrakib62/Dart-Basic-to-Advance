543d086156902e9715c26d9fcdf7dbbf45e8d586
Emranul Hoque <emranrakib01862@gmail.com>
2025-11-19 11:12:19 +0600

    updated

--- bin/Advance_function.dart
+++ bin/Advance_function.dart
@@ -5,6 +5,7 @@ void main(){
 
   anonymousFunctionExample();
   higherorderFunctionExample();
+  recursiveFunctionExample();
 }
 
 void anonymousFunctionExample(){
@@ -36,4 +37,21 @@ void printMessage(){
 Function greetUser(String greeting){
   return(String name)=> print('$greeting,$name');
 }
+
+void recursiveFunctionExample(){
+  print('Factorial of 5 is ${factorial(5)}');
+  print('Fibonacci(6)=${fibonacci(6)}');
+}
+
+int factorial(int n){
+  if(n <=1) return 1;
+  return n*factorial(n-1);
+}
+
+
+int fibonacci(int n){
+  if(n<=1) return n;
+  return fibonacci(n-1) + fibonacci(n-2);
+}
+
 
